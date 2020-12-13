a2875357b0a7dffb26347a9cbde9b012e4d78f69
lens0021 <lorentz0021@gmail.com>
2020-12-13 20:56:44 +0900

    Provide barebone beta feature

--- includes/SpecialAchievementBadges.php
+++ includes/SpecialAchievementBadges.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Extension\AchievementBadges;
 
+use BetaFeatures;
 use SpecialPage;
 
 /**
@@ -16,18 +17,41 @@ class SpecialAchievementBadges extends SpecialPage {
 		parent::__construct( 'AchievementBadges' );
 	}
 
+	/**
+	 * @param string $subPage
+	 */
 	public function execute( $subPage ) {
 		parent::execute( $subPage );
 
 		$output = $this->getOutput();
-		$output->addWikiTextAsInterface( 'Hello, world' );
 
+		if ( !$this->isAchievementEnabled() ) {
+			$output->addWikiTextAsInterface(
+				$this->msg( 'achievementbadges-disabled' )->text()
+			);
+			return;
+		}
+
+		$output->addWikiTextAsInterface( '아직 제공되는 도전과제가 없습니다.' );
 	}
 
+	/**
+	 * @return bool
+	 */
+	public function isAchievementEnabled() {
+		return BetaFeatures::isFeatureEnabled( $this->getUser(), Hooks::ACHIEVEMENT_ENABLE );
+	}
+
+	/**
+	 * @return string
+	 */
 	public function getDescription() {
 		return $this->msg( 'special-achievementbadges' )->text();
 	}
 
+	/**
+	 * @return string
+	 */
 	protected function getGroupName() {
 		return 'users';
 	}
