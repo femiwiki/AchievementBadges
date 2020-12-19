2b76eaa017aab9ac26de1169098b9a0192025c0e
lens0021 <lorentz0021@gmail.com>
2020-12-19 20:20:41 +0900

    Add body text to notifications

--- includes/Notifications/EarnPresentationModel.php
+++ includes/Notifications/EarnPresentationModel.php
@@ -2,42 +2,71 @@
 
 namespace MediaWiki\Extension\AchievementBadges\Notifications;
 
+use EchoEvent;
 use EchoEventPresentationModel;
+use Language;
 use MediaWiki\Extension\AchievementBadges\SpecialAchievements;
 use Message;
 use SpecialPage;
+use User;
 
 class EarnPresentationModel extends EchoEventPresentationModel {
+	/**
+	 * @var array
+	 */
+	private $achievement_key;
+	/**
+	 * @inheritDoc
+	 */
+	protected function __construct(
+		EchoEvent $event,
+		Language $language,
+		User $user,
+		$distributionType
+	) {
+		parent::__construct( $event, $language, $user, $distributionType );
+		$this->achievement_key = $this->event->getExtraParam( 'key' );
+	}
 
 	public function canRender() {
 		return true;
 	}
 
 	/**
-	 * @return string
+	 * @inheritDoc
 	 */
 	public function getIconType() {
 		return 'placeholder';
 	}
 
 	/**
-	 * @return array
+	 * @inheritDoc
 	 */
 	public function getPrimaryLink() {
-		$fragment = 'achievement-' . $this->event->getExtraParam( 'key' );
+		$fragment = 'achievement-' . $this->achievement_key;
 		$title = SpecialPage::getTitleFor( SpecialAchievements::PAGE_NAME, false, $fragment ?? '' );
 		$link = $this->getPageLink( $title, '', true );
 		return $link;
 	}
 
 	/**
-	 * @return Message
+	 * @inheritDoc
 	 */
 	public function getHeaderMessage() : Message {
-		$achievementKey = $this->event->getExtraParam( 'key' );
+		$achievementKey = $this->achievement_key;
 
 		$msg = $this->getMessageWithAgent( 'notification-header-achievementbadges-earn' );
 		$msg->params( $this->msg( 'achievement-name-' . $achievementKey ) );
+		return $msg;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getBodyMessage() {
+		$key = $this->achievement_key;
+		$msg = $this->getMessageWithAgent( "achievement-description-$key" );
+
 		return $msg;
 	}
 }
